551858b62c95c61af05cca1f478d035988a749b5
T-Lind <tiernanxklindauer@gmail.com>
2024-06-14 15:06:18 -0500

    add astra part

--- colbert_get_results.py
+++ colbert_get_results.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 astra_database_id = os.getenv('ASTRA_DATABASE_ID')
 astra_token = os.getenv('ASTRA_TOKEN')
-keyspace = "default_keyspace"
+keyspace = "benchmarking"
 
 database = CassandraDatabase.from_astra(astra_token=astra_token, database_id=astra_database_id, keyspace=keyspace)
 embedding_model = ColbertEmbeddingModel()
@@ -18,10 +18,7 @@ retriever = ColbertRetriever(embedding_model=embedding_model, database=database)
 def retrieve_nearest_documents(query, k=5):
     results = retriever.text_search(query, k=k)
     for i, result in enumerate(results):
-        print(i, result)
-        print(f"Rank: {i + 1} Score: {result[1]}")
-        print(f"Text: {result[0].text[:50]}")
-        print(f"Metadata: {result[0].run}\n")
+        print(int(float(result[0].metadata['index'])))
 
 
 if __name__ == "__main__":
